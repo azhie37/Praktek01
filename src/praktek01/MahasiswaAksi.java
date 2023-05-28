03288e871c090bcfe4ae63e985aa21d59b4c0075
azhie37 <azhie37@gmail.com>
2023-05-28 20:44:46 +0800

    Instance Object

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -9,5 +9,8 @@ package praktek01;
  * @author Azhi
  */
 public class MahasiswaAksi {
-    
+    public static void main(String[] args) {
+        Mahasiswa Harry = new Mahasiswa();
+        
+    }
 }
